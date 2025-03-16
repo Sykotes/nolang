1fd41c0b60040c31830c95bb4be6251a7493a2f1
Sykotes <sykotes@icloud.com>
2025-03-16 18:34:24 +0000

    tidy

--- NoLang.hs
+++ NoLang.hs
@@ -1,5 +1,5 @@
-import Data.Maybe (fromJust)
 import Control.Exception
+import Data.Maybe (fromJust)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
 
@@ -33,57 +33,84 @@ wordAsOp word
   | word == "." = (POP, Nothing)
   | otherwise = (PUSH, Just word)
 
-simulate :: [(Op, Maybe String)] -> Stack -> IO ()
-simulate [] _ = putStr ""
-simulate (x : xs) stack
-  | fst x == POP = do
-      let popped = sPop stack
-      simulate xs $ snd popped
-  | fst x == PUSH = do
-      simulate xs $ sPush (maybeStringToInt (snd x)) stack
-  | fst x == PLUS = do
-      let first_popped = sPop stack
-      let second_popped = sPop $ snd first_popped
-      let a = fst first_popped
-      let b = fst second_popped
+interpret :: [(Op, Maybe String)] -> IO ()
+interpret xs = interpret' xs []
+  where
+    interpret' :: [(Op, Maybe String)] -> Stack -> IO ()
+    interpret' [] _ = putStr ""
+    interpret' (x : xs) stack
+      | fst x == POP = do
+          let popped = sPop stack
+          interpret' xs $ snd popped
+      | fst x == PUSH = do
+          interpret' xs $ sPush (maybeStringToInt (snd x)) stack
+      | fst x == PLUS = do
+          let first_popped = sPop stack
+          let second_popped = sPop $ snd first_popped
+          let a = fst first_popped
+          let b = fst second_popped
 
-      let popped_stack = snd second_popped
-      let new_stack = sPush (b + a) popped_stack
-      simulate xs new_stack
-  | fst x == MINUS = do
-      let first_popped = sPop stack
-      let second_popped = sPop $ snd first_popped
-      let a = fst first_popped
-      let b = fst second_popped
+          let popped_stack = snd second_popped
+          let new_stack = sPush (b + a) popped_stack
+          interpret' xs new_stack
+      | fst x == MINUS = do
+          let first_popped = sPop stack
+          let second_popped = sPop $ snd first_popped
+          let a = fst first_popped
+          let b = fst second_popped
 
-      let popped_stack = snd second_popped
-      let new_stack = sPush (b - a) popped_stack
-      simulate xs new_stack
-  | fst x == PEEK = do
-      print $ sPeek stack
-      simulate xs stack
-  | otherwise = error "Invalid operation"
+          let popped_stack = snd second_popped
+          let new_stack = sPush (b - a) popped_stack
+          interpret' xs new_stack
+      | fst x == PEEK = do
+          print $ sPeek stack
+          interpret' xs stack
+      | otherwise = error "Invalid operation"
 
-parse :: [String] -> [(Op, Maybe String)] -> [(Op, Maybe String)]
-parse xs acc = foldl (\acc x -> acc ++ [wordAsOp x]) acc xs
+compile :: [(Op, Maybe String)] -> IO ()
+compile = error "Compiling not implemented"
+
+parse :: [String] -> [(Op, Maybe String)]
+parse xs = parse' xs []
+  where
+    parse' :: [String] -> [(Op, Maybe String)] -> [(Op, Maybe String)]
+    parse' xs acc = foldl (\acc x -> acc ++ [wordAsOp x]) acc xs
 
 printUsage :: IO ()
-printUsage = putStrLn "Usage: nolang <file>"
+printUsage = do
+  putStrLn "Usage: nolang COMMND <file>"
+  putStrLn "    int <file>      interprets the file"
+  putStrLn "    com <file>      compiles the file"
+
+getValidArgs :: [String] -> (Bool, String)
+getValidArgs args
+  | null args = (False, "")
+  | length args /= 2 = (False, "Wrong amount of arguments")
+  | head args `notElem` ["int", "com"] = (False, "Invalid command")
+  | ' ' `elem` head args = (False, "Invalid file name")
+  | otherwise = (True, "")
+
+run :: [String] -> IO ()
+run args = do
+  let file_path = args !! 1
+  result <- try (readFile file_path) :: IO (Either IOError String)
+  case result of
+    Left err -> do
+      putStrLn $ "Failed to read file - " ++ show err
+    Right content -> do
+      let wordList = words content
+      let command = head args
+      if command == "int"
+        then interpret (parse wordList)
+        else compile (parse wordList)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let file_path = head args
-  if length args /= 1
+  let validArgs = getValidArgs args
+  if not (fst validArgs) 
     then do 
-      putStrLn "Wrong amount of arguments\n"
+      putStr $ snd validArgs
       printUsage
     else do
-      result <- try (readFile file_path) :: IO (Either IOError String)
-      case result of
-        Left err -> do
-          putStrLn $ "Failed to read file - " ++ show err
-        Right content -> do
-          contents <- readFile $ head args
-          let wordList = words contents
-          simulate (parse wordList []) []
+      run args
